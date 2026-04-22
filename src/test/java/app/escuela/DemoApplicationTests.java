8a36eff7c07c38ac9561561acedeac51e7cf76a7
Cristian Moisés Alvarado Campos <Cristts2.0@gmail.com>
2026-04-22 17:14:02 -0600

    iniciando

---
+++ src/test/java/app/escuela/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package app.escuela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
